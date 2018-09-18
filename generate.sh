ee41a3c7d9fa8f0aff1ddaae50995523a27daa18
Simon Renoult <srenoult@octo.com>
2018-09-18 13:43:39 +0200

    1.0.2

--- generate.sh
+++ generate.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-git clone git@github.com:simonrenoult/express-with-superpowers.git $1
+git clone git@github.com:simonrenoult/express-with-superpowers.git ./foobar
+cd ./foobar
+rm -rf .git
+git init && git add --all && git commit --message="Initial commit"
